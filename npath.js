1479430c59503fd3dc62ad6948ccecb736646930
Rahul Srivastava <himsln28@gmail.com>
2023-07-26 23:28:54 +0530

    initial commit

---
+++ npath.js
@@ -0,0 +1,9 @@
+function openAppointmentForm() {
+    window.open("https://forms.gle/RLDNE968QVNp23GUA", "_blank");
+}
+
+function toggleNavbar() {
+    var navbarLinks = document.getElementById("navbarLinks");
+    navbarLinks.classList.toggle("active");
+
+}
